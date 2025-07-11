70abf87b368bdbf9d988aeadd48e8a462ceccbe5
NguyenNhutQuoc <nguyennhutquoc1412@gmail.com>
2025-07-11 14:44:48 +0700

    Update logo and name app

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,6 +1,6 @@
 // src/constants/config.ts - Updated with dark mode support
 // API configuration
-export const API_URL = "https://31318be7e464.ngrok-free.app/api/v1"; // Development server
+export const API_URL = "https://trucking-app-api.vercel.app/api/v1"; // Development server
 // export const API_URL = 'https://api.example.com/api'; // Production server
 console.log("API URL:", API_URL);
 // App configuration
